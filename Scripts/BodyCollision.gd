5525f8c6447c90e38464ba3fb7fa667fd5d8e737
Klaudia Sawicka <sawicka.klaudi@gmail.com>
2022-02-04 16:42:59 +0100

    Small script changes

--- Scripts/BodyCollision.gd
+++ Scripts/BodyCollision.gd
@@ -20,5 +20,5 @@ func _on_Portal4_body_shape_entered(_body_id, _body,_body_shape, _local_shape):
 	get_tree().change_scene("res://Scenes/Level5.tscn")
 
 
-func _on_Portal5_body_shape_entered(body_id, body, body_shape, local_shape):
+func _on_Portal5_body_shape_entered(_body_id, _body,_body_shape, _local_shape):
 	get_tree().change_scene("res://Scenes/ScenePicker.tscn")
